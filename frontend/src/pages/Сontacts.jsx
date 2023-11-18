9aaa1fde7e43a71ee17576f5e4fc8e1522a1dc04
Namo333 <dad688634@gmail.com>
2023-11-19 00:02:15 +0300

    e

--- frontend/src/pages/Сontacts.jsx
+++ frontend/src/pages/Сontacts.jsx
@@ -4,8 +4,8 @@ import { HeroHome, Info} from './components';
 
 const Сontacts = () => (
     <section className='py-[200px]'>
-        <h1 className='max-w-[1000px] text-black text-5xl font-normal leading-[62px] m-0 p-0'>С предложениями по заказам писать сюда:</h1>
-        <a href="https://t.me/zverkovdsg" class='text-indigo-800 text-8xl font-semibold leading-[124px] m-0 p-0'>t.me/zverkovdsg</a>
+        <h1 className='max-w-[1000px] text-black text-5xl max-md:text-[50px] max-sm:text-[30px] font-normal m-0 p-0'>С предложениями по заказам писать сюда:</h1>
+        <a href="https://t.me/zverkovdsg" class="text-indigo-800 font-semibold leading-[124px] m-0 p-0 md:text-8xl sm:text-6xl max-sm:text-[30px]">t.me/zverkovdsg</a>
     </section>
 );
 
